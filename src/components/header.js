bafae3ab9c330ff68ac5f49c874b861e2bb2a932
Code <Code@batc.local>
2018-07-16 09:35:03 -0600

    Created Content component to generate story

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div classname="header">
+        <div className="header">
         <h1>Bottega MadLibs</h1>
         <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
         </div>
--- src/components/input.js
+++ src/components/input.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Input = (title, state, onChange, name) => {
+const Input = ({title, state, name}, onChange) => {
     return (
-        <div classname="input">
+        <div key={name} className="input">
         <input name={name} value={state} onChange={onChange}/>
         <label>{title}</label>
         </div>
